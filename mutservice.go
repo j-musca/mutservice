7c28345b62dbb0d26f767f56113041dfe5e0b882
Jonas Mücke <jonas.muecke@neofonie.de>
2016-04-07 16:51:49 +0200

    added dummy email and cron trigger

--- mutservice.go
+++ mutservice.go
@@ -10,31 +10,124 @@ import (
 	"log"
 	"os"
 	"time"
+	"crypto/sha1"
+	"encoding/hex"
+	"strings"
+	"io/ioutil"
+	"github.com/ddliu/go-httpclient"
+	"github.com/robfig/cron"
 )
 
-func main() {
-	db, boltErr := bolt.Open(os.Getenv("HOME")+"/app-mut.db", 0600, &bolt.Options{Timeout: 1 * time.Second})
+var USERS_BUCKET []byte = []byte("users")
+var KEYS_BUCKET []byte = []byte("keys")
+var MOODS_BUCKET []byte = []byte("moods")
+var BUCKET_NAMES [][]byte = [][]byte{USERS_BUCKET, KEYS_BUCKET, MOODS_BUCKET}
 
-	if boltErr != nil {
-		log.Fatal(boltErr)
+func main() {
+	dataBase, dataBaseError := bolt.Open(os.Getenv("HOME")+"/app-mut.db", 0600, &bolt.Options{Timeout: 1 * time.Second})
+
+	if dataBaseError != nil {
+		log.Fatal(dataBaseError)
 	}
 
-	defer db.Close()
+	defer dataBase.Close()
 
-	db.Update(createBuckets)
+	scheduler := cron.New()
 
-	serverError := fasthttp.ListenAndServe(":8081", createRouter(db).Handler)
+	scheduler.AddFunc("14 30 * * * *", triggerMail(dataBase))
+
+	dataBase.Update(createBuckets)
+
+	serverError := fasthttp.ListenAndServe(":8081", createRouter(dataBase).Handler)
 
 	if serverError != nil {
 		log.Fatalf("Error in ListenAndServe: %s", serverError)
 	}
 }
 
-func createBuckets(tx *bolt.Tx) error {
-	bucketNames := [3]string{"users", "keys", "moods"}
+func sendMail(email string, text string) {
+	response, responseError := httpclient.
+	WithHeader("Authorization", "Basic YXBpOmtleS00NWY0ODYxNTVkZmUzZjUxY2ExOTg4MjEwNGIzYmViMg==").
+	Post("https://api.mailgun.net/v3/sandbox4ebeef9e81ca4130885ef51fa4b9729f.mailgun.org/messages", map[string]string {
+		"from": "Mailgun Sandbox <postmaster@sandbox4ebeef9e81ca4130885ef51fa4b9729f.mailgun.org>",
+		"to": email,
+		"subject": "How is your mood today?",
+		"text": text,
+	})
 
-	for _, element := range bucketNames {
-		_, err := tx.CreateBucketIfNotExists([]byte(element))
+	if responseError != nil {
+		fmt.Printf("%s", responseError)
+	}
+
+	defer response.Body.Close()
+
+	_, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+}
+
+func triggerMail(db *bolt.DB) {
+	return func() {
+		users, triggerError := getUsers(db)
+
+		if triggerError != nil {
+			fmt.Printf("%s", triggerError)
+		}
+
+		mailTasks, triggerError := createMailTasks(users, db)
+
+		if triggerError != nil {
+			fmt.Printf("%s", triggerError)
+		}
+
+		sendMails(mailTasks)
+	}
+}
+
+func sendMails(tasks []MailTask) {
+	for _, task := range tasks {
+		sendMail(task.Email, "" + task.Key)
+	}
+}
+
+func createMailTasks(users []User, db *bolt.DB) (tasks []MailTask, dbError error) {
+	today := time.Now().Format("02-01-2006")
+
+	dbError = db.Update(func(tx *bolt.Tx) error {
+		tx.DeleteBucket(KEYS_BUCKET)
+		bucket, _ := tx.CreateBucket(KEYS_BUCKET)
+
+		for _, user := range users {
+			key := createKey(user.Uuid, today)
+
+			bucketError := bucket.Put([]byte(key), []byte(today))
+
+			if bucketError != nil {
+				return bucketError
+			}
+
+			tasks = append(tasks, MailTask{user.Email, key})
+		}
+
+		return nil
+	})
+
+	return tasks, dbError
+}
+
+func createKey(uuid string, dateString string) (key string) {
+	source := strings.Join([]string{uuid, dateString}, "-")
+	hashCreator := sha1.New()
+	hashCreator.Write([]byte(source))
+	key = hex.EncodeToString(hashCreator.Sum(nil))
+
+	return key
+}
+
+func createBuckets(tx *bolt.Tx) error {
+	for _, element := range BUCKET_NAMES {
+		_, err := tx.CreateBucketIfNotExists(element)
 
 		if err != nil {
 			return fmt.Errorf("Could not create bucket: %s", err)
@@ -80,7 +173,7 @@ func getUsersRequest(db *bolt.DB) fasthttprouter.Handle {
 
 func getUsers(db *bolt.DB) (users []User, dbError error) {
 	dbError = db.View(func(tx *bolt.Tx) error {
-		usersBucket := tx.Bucket([]byte("users"))
+		usersBucket := tx.Bucket(USERS_BUCKET)
 		usersBucket.ForEach(func(uuid []byte, email []byte) error {
 			users = append(users, User{string(uuid), string(email)})
 			return nil
@@ -121,8 +214,7 @@ func getUserByUuidRequest(db *bolt.DB) fasthttprouter.Handle {
 
 func getUserByUuid(db *bolt.DB, uuid string) (user *User, dbError error) {
 	dbError = db.View(func(tx *bolt.Tx) error {
-		usersBucket := tx.Bucket([]byte("users"))
-
+		usersBucket := tx.Bucket(USERS_BUCKET)
 		email := usersBucket.Get([]byte(uuid))
 
 		if email != nil {
@@ -167,11 +259,16 @@ func saveUser(db *bolt.DB, userCreation UserCreation) (user User, err error) {
 	uuid, _ := uuid.NewV4()
 
 	dbError := db.Update(func(tx *bolt.Tx) error {
-		bucket := tx.Bucket([]byte("users"))
+		bucket := tx.Bucket(USERS_BUCKET)
 		return bucket.Put([]byte(uuid.String()), []byte(userCreation.Email))
 	})
 
 	return User{uuid.String(), userCreation.Email}, dbError
+}
+
+type FeedbackKey struct {
+	Key string
+	DateString string
 }
 
 type User struct {
@@ -185,4 +282,9 @@ type UsersResponse struct {
 
 type UserCreation struct {
 	Email string `json:"email"`
+}
+
+type MailTask struct {
+	Email string
+	Key string
 }
